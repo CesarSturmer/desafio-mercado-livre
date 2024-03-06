7537af9f4949844465e1159c26584f588437ea3f
Cesar Sturmer <cesar.sturmer@fcamara.com>
2024-03-06 16:19:34 -0300

    feat: adjusted responsive page and other fixs

--- src/app/[search]/page.tsx
+++ src/app/[search]/page.tsx
@@ -3,15 +3,20 @@ import { FormattedData } from "@/types/responseItems/index.type";
 import ListProducts from "@/components/ListProducts";
 
 async function getProduct(query: string) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL_API}/items?q=${query}`
-  );
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL_API}/items?q=${query}`
+    );
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    if (!res.ok) {
+      throw new Error("Failed to fetch data search");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.error("There was an error fetching the search products", error);
+    return null;
   }
-
-  return res.json();
 }
 
 export default async function ProductItems({
